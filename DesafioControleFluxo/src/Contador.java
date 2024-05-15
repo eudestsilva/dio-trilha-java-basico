0c87eac24df527e579c836daba3abde8e9335272
eudes tine silva <eudestsilva@gmail.com>
2024-05-15 10:48:00 -0300

    Projeto concluido

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class Contador {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+	public static void main(String[] args) throws Exception {
+	/* receber parâmetros via terminal retorna inteiros*/
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Digite o primeiro número:");
+		int parametroUm = teclado.nextInt();
+		System.out.println("Digite o segundo número:");
+		int parametroDois = teclado.nextInt();
+			
+		try {
+			contar(parametroUm, parametroDois);
+            System.out.println("O programa foi finalizado");
+		}
+		catch (ParametrosInvalidosException exception) {
+            //lançando exceção
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+		}
     }
-}
+
+	static void contar(int parametroUm,int parametroDois) throws ParametrosInvalidosException {
+		
+        if (parametroUm > parametroDois) {
+			throw new ParametrosInvalidosException();
+        }
+	
+			int contagem = parametroDois - parametroUm;
+			for(int i = 0; i <= contagem; i++) {
+				System.out.println(i);
+        }
+	}
+
+    }
+  
